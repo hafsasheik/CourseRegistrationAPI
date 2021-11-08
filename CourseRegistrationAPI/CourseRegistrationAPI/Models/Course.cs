7a875a5949a4ba2d2f56a2b83f7be5f0b5e37a8d
nidjuru <eenidjo@gmail.com>
2021-11-08 17:37:17 +0100

    Models implemented

---
+++ CourseRegistrationAPI/CourseRegistrationAPI/Models/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseRegistrationAPI.Models
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string Subject { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int StudyPace { get; set; }
+
+    }
+}
---
+++ CourseRegistrationAPI/CourseRegistrationAPI/Models/Registration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseRegistrationAPI.Models
+{
+    public class Registration
+    {
+        public int RegistrationId { get; set; }
+        public ICollection<User> Users { get; set; }
+        public ICollection<Course> Courses { get; set; }
+    }
+}
