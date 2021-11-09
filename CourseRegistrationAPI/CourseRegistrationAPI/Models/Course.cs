b44be170e79c717f8f22645b64d1d9db3d4eed1e
nidjuru <eenidjo@gmail.com>
2021-11-09 11:12:15 +0100

    DataAnnotations

--- CourseRegistrationAPI/CourseRegistrationAPI/Models/Course.cs
+++ CourseRegistrationAPI/CourseRegistrationAPI/Models/Course.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,16 @@ namespace CourseRegistrationAPI.Models
 {
     public class Course
     {
+        [Key]
         public int CourseId { get; set; }
         public string Subject { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd MMM yyyy}")]
+
         public DateTime StartDate { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd MMM yyyy}")]
+
         public DateTime EndDate { get; set; }
+        [Required]
         public int StudyPace { get; set; }
         public ICollection<Registration> Registrations { get; set; }
     }
--- CourseRegistrationAPI/CourseRegistrationAPI/Models/User.cs
+++ CourseRegistrationAPI/CourseRegistrationAPI/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,18 @@ namespace CourseRegistrationAPI.Models
 {
     public class User
     {
+        [Key]
         public int UserId { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string FirstName { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string LastName { get; set; }
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string Password { get; set; }
         public ICollection<Registration> Registrations { get; set; }
 
