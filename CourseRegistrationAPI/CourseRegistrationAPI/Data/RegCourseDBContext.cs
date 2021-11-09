4962e3bb1655b1abc61364ca880a6b853a18e15e
hafsasheik <70884095+hafsasheik@users.noreply.github.com>
2021-11-09 13:39:00 +0100

    Påbörjat DBCOntext och la till CORS

---
+++ CourseRegistrationAPI/CourseRegistrationAPI/Data/RegCourseDBContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CourseRegistrationAPI.Models;
+
+namespace CourseRegistrationAPI.Data
+{
+    public class RegCourseDBContext: DbContext
+    {
+        public RegCourseDBContext(DbContextOptions options)
+        : base(options)
+        {
+
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Registration> Registrations { get; set; }
+        public DbSet<User> Users { get; set; }
+
+
+
+
+    }
+
+
+
+}
--- CourseRegistrationAPI/CourseRegistrationAPI/Startup.cs
+++ CourseRegistrationAPI/CourseRegistrationAPI/Startup.cs
@@ -1,7 +1,9 @@
+using CourseRegistrationAPI.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,13 +28,23 @@ namespace CourseRegistrationAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<RegCourseDBContext>(opts =>
+            opts.UseSqlServer(Configuration.GetConnectionString("sqlConnection")));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CourseRegistrationAPI", Version = "v1" });
             });
+
+            // Enable CORS - apps from other domains to access the api
+            services.AddCors(c =>
+            {
+                c.AddPolicy("AllowOrigin", option => option.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            });
         }
+
+
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -42,6 +54,15 @@ namespace CourseRegistrationAPI
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CourseRegistrationAPI v1"));
+            }
+
+            //Use CORS in pipeline
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+
             }
 
             app.UseHttpsRedirection();
