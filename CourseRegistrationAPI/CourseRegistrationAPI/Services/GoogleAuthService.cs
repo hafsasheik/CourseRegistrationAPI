549b00bd60432ecf4cb5c15d8b866582f2dc019e
Erik Sundberg <sten.erik.sundberg@gmail.com>
2021-11-11 13:10:51 +0100

    Google-login implementerat, men otestat.

---
+++ CourseRegistrationAPI/CourseRegistrationAPI/Services/GoogleAuthService.cs
@@ -0,0 +1,42 @@
+﻿using CourseRegistrationAPI.Data;
+using CourseRegistrationAPI.Models;
+using Google.Apis.Auth;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseRegistrationAPI.Services
+{
+    public class GoogleAuthService : IGoogleAuthService
+    {
+        private readonly RegCourseDBContext _context;
+
+        public GoogleAuthService(RegCourseDBContext context)
+        {
+            _context = context;
+            
+        }
+
+        public async Task<User> AuthenticateUserAsync(string googleToken)
+        {
+            try
+            {
+                var payload = await GoogleJsonWebSignature.ValidateAsync(googleToken, new GoogleJsonWebSignature.ValidationSettings());
+                User u = await _context.Users.FirstOrDefaultAsync(u => u.Email == payload.Email);
+
+                return u;
+            }
+            catch(Exception epicFail)
+            {
+                Debug.WriteLine(epicFail.Message);
+                return null;
+            }
+            
+
+            
+        }
+    }
+}
